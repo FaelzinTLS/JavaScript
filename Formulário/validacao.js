78998de8f801b712890baff7426ed72d5d6e213d
RAFAEL DOS SANTOS GONCALVES <rafael.sgoncalves7@senacsp.edu.br>
2024-08-19 21:46:10 -0300

    arquivos

---
+++ Formulário/validacao.js
@@ -0,0 +1,26 @@
+// Função para validar o formulário
+function validarFormulario(){
+    const nome = document.getElementById("nome").value;
+    const email = document.getElementById("email").value;
+    const senha = document.getElementById("senha").value;
+    const mensagemErro = document.getElementById("mensagemErro");
+
+    if(nome === ""){
+        mensagemErro.innerHTML = "Por favor, preencha o campo Nome.";
+        return false;
+    }
+    
+    if(email === ""){
+        mensagemErro.innerHTML = "Por favor, preencha o campo E-mail.";
+        return false;
+    }
+
+    if(senha === ""){
+        mensagemErro.innerHTML = "Por favor, preencha o campo Senha.";
+        return false;
+    }
+
+    return true;
+
+
+}
